d8cbde0ebadaa53e8148c978286c254e67661251
tomo <yonetomo05166@gmasil.com>
2020-11-10 18:32:35 +0900

    complete

--- src/models/validators/TaskValidator.java
+++ src/models/validators/TaskValidator.java
@@ -6,7 +6,7 @@ import java.util.List;
 import models.Task;
 
 public class TaskValidator {
-    // バリデーションを実行する
+
     public static List<String> validate(Task t) {
         List<String> errors = new ArrayList<String>();
 
@@ -21,8 +21,6 @@ public class TaskValidator {
         return errors;
     }
 
-
-    // メッセージの必須入力チェック
     private static String _validateContent(String content) {
         if(content == null || content.equals("")) {
             return "タスクを入力してください。";
